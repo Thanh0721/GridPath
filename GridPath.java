b3ad41dd84de9e4f8560635367e883020c4f07d7
Thanh0721 <thanhn2789@nycstudents.net>
2024-10-23 09:29:39 -0400

    Update GridPath.java

--- GridPath.java
+++ GridPath.java
@@ -1,6 +1,47 @@
-public class GridPath {
+public class GridPath 
+{
+    public GridPath(int[][] values)
+    {
+        gid = values;
+    }
+    
+
+    public int sumPath(int row, int col)
+    {
+        return 0;
+    }
+
     /** Initialized in the constructor with distinct values that never change */
     private int[][] grid;
+
+    public String toString()
+    {
+        String s = "";
+        for (int row = 0; row < grid.length; row++)
+        {
+            for (int col = 0; col < grid[0].length; col++)
+            {
+                s += grid[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    public String toString()
+    {
+        String s = "";
+        for (int[] row : grid)
+        {
+            for (int col : row)
+            {
+                s += col + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
 
     /**
      * Returns the Location representing a neighbor of the grid element at row and
@@ -11,18 +52,8 @@ public class GridPath {
      * row and col do not specify the element in the last row and last column of
      * grid.
      */
-    public Location getNextLoc(int row, int col) {
-        /* to be implemented in part (a) */ }
-
-    /**
-     * Computes and returns the sum of all values on a path through grid, as
-     * described in
-     * part (b)
-     * Preconditions: row is a valid row index and col is a valid column index in
-     * grid.
-     * row and col do not specify the element in the last row and last column of
-     * grid.
-     */
-    public int sumPath(int row, int col) {
-        /* to be implemented in part (b) */ }
+    public Location getNextLoc(int row, int col) 
+    {
+        return null;
+    }
 }
