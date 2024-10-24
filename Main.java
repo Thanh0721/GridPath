fd0afa04e42266ce33b0f04d1f13af0e9f4e6f9b
Thanh0721 <thanhn2789@nycstudents.net>
2024-10-24 09:26:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,9 @@ public class Main
             {10, 17, 20, 19, 1},
             {18, 22, 30, 25, 6}
         };
+
         GridPath one = new GridPath(nums);
+        
         System.out.println(one);
     }
 }
