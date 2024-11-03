c365cac170f6599b7cbf5c89bb191bab93d351ec
Thanh0721 <thanhn2789@nycstudents.net>
2024-11-03 17:47:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,33 @@
-public class Main
-{
-    public static void main(String[] args)
+public class Main {
+    public static void main(String[] args) 
     {
-        int[][] nums = 
+        int[][] grid = 
         {
-            {12, 3, 4, 13, 5},
-            {11, 21, 2, 14, 16},
-            {7, 8, 9, 15, 0},
-            {10, 17, 20, 19, 1},
-            {18, 22, 30, 25, 6}
+            {4, 8, 2, 3},
+            {6, 1, 9, 5},
+            {7, 3, 4, 6},
+            {9, 6, 2, 8}
         };
 
-        GridPath one = new GridPath(nums);
+        GridPath gridPath = new GridPath(grid);
+
+        int startRow = 0;
+        int startCol = 0;
+
+        int totalSum = gridPath.sumPath(startRow, startCol);
+        System.out.println("Total path sum starting from (0, 0): " + totalSum);
+
+        System.out.println("Path taken from (0, 0):");
+        int row = startRow;
+        int col = startCol;
+        while (row < grid.length - 1 || col < grid[0].length - 1) 
+        {
+            System.out.println("Current location: (" + row + ", " + col + ") with value: " + grid[row][col]);
+            Location nextLoc = gridPath.getNextLoc(row, col);
+            row = nextLoc.getRow();
+            col = nextLoc.getCol();
+        }
         
-        System.out.println(one);
+        System.out.println("Final location: (" + row + ", " + col + ") with value: " + grid[row][col]);
     }
 }
