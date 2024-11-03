616d2d4a6b44c3fc4d8c4dd64ca4bd0c097fa794
Thanh0721 <thanhn2789@nycstudents.net>
2024-11-03 17:48:15 -0500

    Update GridPath.java

--- GridPath.java
+++ GridPath.java
@@ -2,23 +2,52 @@ public class GridPath
 {
     private int[][] grid;
 
-    public GridPath(int[][] values)
+    public GridPath(int[][] grid) 
     {
-        grid = values;
+        this.grid = grid;
     }
 
-    @Override
-    public String toString()
+    public Location getNextLoc(int row, int col) 
     {
-        StringBuilder s = new StringBuilder();
-        for (int[] row : grid)
+        int numRows = grid.length;
+        int numCols = grid[0].length;
+        int belowRow = row + 1;
+        int rightCol = col + 1;
+
+        boolean hasBelow = belowRow < numRows;
+        boolean hasRight = rightCol < numCols;
+
+        if (!hasBelow && !hasRight) 
         {
-            for (int col : row)
+            return null;
+        } else if (!hasBelow) {
+            return new Location(row, rightCol);
+        } else if (!hasRight) {
+            return new Location(belowRow, col);
+        } 
+        else 
+        {
+            if (grid[belowRow][col] <= grid[row][rightCol]) 
             {
-                s.append(col).append(" ");
+                return new Location(belowRow, col);
+            } else {
+                return new Location(row, rightCol);
             }
-            s.append("\n");
         }
-        return s.toString();
+    }
+
+    public int sumPath(int row, int col) 
+    {
+        int sum = 0;
+        while (row < grid.length - 1 || col < grid[0].length - 1) 
+        {
+            sum += grid[row][col];
+            Location nextLoc = getNextLoc(row, col);
+            row = nextLoc.getRow();
+            col = nextLoc.getCol();
+        }
+
+        sum += grid[row][col];
+        return sum;
     }
 }
